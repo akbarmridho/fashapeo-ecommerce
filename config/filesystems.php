56cbca7f20399e72681781b9c2cedc3ff59bd9b4
Akbar Maulana Ridho <akbarmaulanaridho1@gmail.com>
2021-02-13 16:00:14 +0700

    .

--- config/filesystems.php
+++ config/filesystems.php
@@ -55,7 +55,7 @@ return [
         'gcs' => [
             'driver' => 'gcs',
             'project_id' => env('GOOGLE_CLOUD_PROJECT_ID', 'your-project-id'),
-            'key_file' => json_decode(env('GOOGLE_CLOUD_KEY_FILE', null)) ?: env('GOOGLE_CLOUD_KEY_FILE'),
+            'key_file' => env('GOOGLE_CLOUD_KEY_FILE', null),
             'bucket' => env('GOOGLE_CLOUD_STORAGE_BUCKET', 'your-bucket'),
             'path_prefix' => env('GOOGLE_CLOUD_STORAGE_PATH_PREFIX', null), // optional: /default/path/to/apply/in/bucket
             'storage_api_uri' => env('GOOGLE_CLOUD_STORAGE_API_URI', null), // see: Public URLs below
